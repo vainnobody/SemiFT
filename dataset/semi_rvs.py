4d78b5f8626228a68f5c3a35275962529d4e3d71
Lan Jie <1355008426@qq.com>
2026-01-26 16:42:27 +0800

    add file : fixmatch_rgcr, fixmatch_rgcr_moe

---
+++ dataset/semi_rvs.py
@@ -0,0 +1,106 @@
+import os
+import math
+import random
+import numpy as np
+from PIL import Image
+from copy import deepcopy
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from dataset.transform import *
+import torchvision.transforms.functional as TF
+
+
+
+
+
+class SemiDataset(Dataset):
+    def __init__(self, name, root, mode, size=None, ignore_value=5, id_path=None, nsample=None):
+        self.name = name
+        self.root = root
+        self.mode = mode
+        self.size = size
+        self.ignore_value = ignore_value
+        self.reduce_zero_label = True if name == 'ade20k' else False
+
+        if mode == 'train_l' or mode == 'train_u':
+            with open(id_path, 'r') as f:
+                self.ids = f.read().splitlines()
+            if mode == 'train_l' and nsample is not None:
+                self.ids *= math.ceil(nsample / len(self.ids))
+                self.ids = self.ids[:nsample]
+        else:
+            with open('splits/%s/val.txt' % name, 'r') as f:
+                self.ids = f.read().splitlines()
+
+    def process_mask(self, mask):
+        mask = np.array(mask) - 1
+        return Image.fromarray(mask)
+
+    def __getitem__(self, item):
+        id = random.choice(self.ids)
+        img_ori = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
+       #  mask_ori = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
+
+        if self.mode == 'train_u':
+            mask_ori = Image.fromarray(np.zeros((img_ori.size[1], img_ori.size[0]), dtype=np.uint8))
+        else:
+            mask_ori = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1])))) 
+            if self.name == 'loveda': 
+                mask_ori = self.process_mask(mask_ori) 
+
+        ignore_value = 254 if self.mode == 'train_u' else 5
+        
+        img, mask, x, y = crop_with_xy(img_ori, mask_ori, self.size, ignore_value)
+        
+        min_theta = 0.0
+        max_theta = 360.
+        theta = np.random.uniform(min_theta, max_theta)
+        # theta = np.random.choice([90.0, 180.0, 270.0])
+        
+        s = float(np.random.choice([0.5, 0.75, 1.0, 1.25, 1.5, 2.0]))
+        
+        img_c, mask_c, x_c, y_c = context_crop(img_ori, mask_ori, self.size, ignore_value, x, y ,s)
+        
+        img_c = img_c.resize(img.size)
+        mask_c = mask_c.resize(img.size, resample=Image.Resampling.NEAREST)
+        
+        if self.mode == 'train_l':
+            img_c = TF.rotate(img_c, angle=theta, interpolation=TF.InterpolationMode.BILINEAR)
+            mask_c = TF.rotate(mask_c, angle=theta, interpolation=TF.InterpolationMode.NEAREST, fill=0)
+            img, mask = normalize(img, mask)
+            img_c, mask_c = normalize(img_c, mask_c)
+            return img,mask,img_c, mask_c
+
+        img_w, img_s1, img_s2 = deepcopy(img), deepcopy(img), deepcopy(img_c)
+
+        if random.random() < 0.8:
+            img_s1 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s1)
+        img_s1 = transforms.RandomGrayscale(p=0.2)(img_s1)
+        img_s1 = blur(img_s1, p=0.5)
+        cutmix_box1 = obtain_cutmix_box(img_s1.size[0], p=0.5)
+
+        if random.random() < 0.8:
+            img_s2 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s2)
+        img_s2 = transforms.RandomGrayscale(p=0.2)(img_s2)
+        img_s2 = blur(img_s2, p=0.5)
+        cutmix_box2 = obtain_cutmix_box(img_s2.size[0], p=0.5)
+
+        ignore_mask = Image.fromarray(np.zeros((mask.size[1], mask.size[0])))
+
+        mask_c = torch.ones((1, self.size, self.size))
+
+        img_s1, ignore_mask = normalize(img_s1, ignore_mask)
+        img_s2 = normalize(img_s2)
+
+        mask = torch.from_numpy(np.array(mask)).long()
+        ignore_mask[mask == 254] = 255
+
+        img_s2 = TF.rotate(img_s1, angle=theta, interpolation=TF.InterpolationMode.BILINEAR)
+        mask_rotated = TF.rotate(mask_c, angle=theta, interpolation=TF.InterpolationMode.NEAREST, fill=0)
+
+        return normalize(img_w), img_s1, img_s2, ignore_mask, cutmix_box1, cutmix_box2, torch.tensor([x,y,x_c,y_c,s,theta]), mask_rotated
+    def __len__(self):
+        # return 1000
+        return len(self.ids) * 50
---
+++ fixmatch_rvsc.py
@@ -0,0 +1,474 @@
+import os
+import time
+import yaml
+import pprint
+import logging
+import argparse
+import random
+import torch
+import torch.backends.cudnn as cudnn
+from torch import nn
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from dataset.semi_rvs import SemiDataset
+from dataset.val import ValDataset
+from model.semseg.deeplabv3plus import DeepLabV3Plus
+from util.classes import CLASSES
+from util.ohem import ProbOhemCrossEntropy2d
+from util.utils import count_params, init_log, AverageMeter
+from util.dist_helper import setup_distributed
+from util.train_utils import (
+    DictAverageMeter,
+    confidence_weighted_loss,
+    cutmix_img_,
+    cutmix_mask,
+)
+from supervised import validation_cpu
+
+# from model.semseg.dpt import DPT
+# from model.semseg.upernet_dinov2 import UperNet
+from model.semseg.upernet_single import UperNet
+# from model.backbone.dinov2 import DINOv2
+
+import torchvision.transforms.functional as TF
+import torch.nn.functional as F
+
+parser = argparse.ArgumentParser(description="Semi-Supervised Semantic Segmentation")
+parser.add_argument("--config", type=str, required=True)
+parser.add_argument("--backbone", type=str, default="swint", required=True)
+parser.add_argument("--init_backbone", type=str, default="imp", required=True)
+parser.add_argument("--decoder", type=str, default="upernet", required=True)
+parser.add_argument("--labeled-id-path", type=str, required=True)
+parser.add_argument("--unlabeled-id-path", type=str, required=True)
+parser.add_argument("--save-path", type=str, required=True)
+parser.add_argument("--local_rank", default=0, type=int)
+parser.add_argument("--port", default=None, type=int)
+parser.add_argument("--load", type=str, default="backbone")
+parser.add_argument("--interval", type=int, default=1)
+
+
+def set_seeds(seed=2024):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def scale_back(pred_c_back, mask_c_back, size, box):
+    B, C, h, w = pred_c_back.shape
+
+    preds = []
+    masks = []
+    mask_s = []
+
+    for i in range(B):
+        x, y, x_c, y_c, s, theta = box[i]
+        x, y, x_c, y_c, s, theta = (
+            x.item(),
+            y.item(),
+            x_c.item(),
+            y_c.item(),
+            s.item(),
+            theta.item(),
+        )
+
+        pred = TF.rotate(
+            pred_c_back[i], angle=-theta, interpolation=TF.InterpolationMode.BILINEAR
+        )
+        mask = TF.rotate(
+            mask_c_back[i], angle=-theta, interpolation=TF.InterpolationMode.NEAREST,fill=0
+        )
+        aligned_ctx = torch.zeros((C, h, w)).to(pred_c_back.device)
+        aligned_mask = torch.zeros((1, h, w)).to(pred_c_back.device)
+        rect_m = [x,y,x+size, y+size]
+        rect_s = [x_c, y_c, x_c +size *s,y_c+size*s]
+
+        inter_x1 = max(rect_m[0], rect_s[0])
+        inter_y1 = max(rect_m[1], rect_s[1])
+        inter_x2 = min(rect_m[2], rect_s[2])
+        inter_y2 = min(rect_m[3], rect_s[3])
+
+        m_x1 = int(round(inter_x1 - x))
+        m_y1 = int(round(inter_y1 - y))
+        m_x2 = int(round(inter_x2 - x))
+        m_y2 = int(round(inter_y2 - y))
+
+        target_h = m_y2 - m_y1
+        target_w = m_x2 - m_x1
+
+        c_x1 = int(round((inter_x1 - x_c) / s))
+        c_y1 = int(round((inter_y1 - y_c) / s))
+        c_x2 = int(round((inter_x2 - x_c) / s))
+        c_y2 = int(round((inter_y2 - y_c) / s))
+
+        c_x1, c_x2 = max(0, c_x1), min(size, c_x2)
+        c_y1, c_y2 = max(0, c_y1), min(size, c_y2)
+
+        pred_patch = pred[:, c_y1:c_y2, c_x1:c_x2]
+        mask_patch = mask[:, c_y1:c_y2, c_x1:c_x2]
+
+        pred_patch = F.interpolate(
+            pred_patch.unsqueeze(0), size=(target_h, target_w), mode="bilinear"
+        ).squeeze(0)
+
+        mask_patch = F.interpolate(
+            mask_patch.unsqueeze(0), size=(target_h, target_w), mode="nearest"
+        ).squeeze(0)
+
+        # print(mask_patch.shape)
+        aligned_ctx[:, m_y1:m_y2, m_x1:m_x2] = pred_patch
+        aligned_mask[:, m_y1:m_y2, m_x1:m_x2] = mask_patch
+
+        preds.append(aligned_ctx)
+        masks.append(aligned_mask)
+
+    return torch.stack(preds, dim=0), torch.stack(masks, dim=0)
+
+
+def main():
+    args = parser.parse_args()
+
+    cfg = yaml.load(open(args.config, "r"), Loader=yaml.Loader)
+
+    logger = init_log("global", logging.INFO)
+    logger.propagate = 0
+
+    rank, world_size = setup_distributed(port=args.port)
+    ddp = True if world_size > 1 else False
+    amp = cfg["amp"]
+
+    if rank == 0:
+        all_args = {**cfg, **vars(args), "ngpus": world_size}
+        logger.info("{}\n".format(pprint.pformat(all_args)))
+
+        writer = SummaryWriter(args.save_path)
+
+        os.makedirs(args.save_path, exist_ok=True)
+
+    cudnn.enabled = True
+    cudnn.benchmark = True
+
+    conf_thresh = cfg["conf_thresh"]
+    # model_configs = {
+    #     'small': {'encoder_size': 'small', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+    #     'base': {'encoder_size': 'base', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+    #     'large': {'encoder_size': 'large', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+    #     'giant': {'encoder_size': 'giant', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+    # }
+    # backbone = DINOv2(model_name='base')
+    # state_dict = torch.load(f'/data1/users/lvliang/project_123/S5_finetune/pretrained/{cfg["backbone"]}.pth')
+    # backbone.load_state_dict(state_dict)
+    # model = UperNet(args, cfg, backbone)
+    model = UperNet(args, cfg)
+    # model = DeepLabV3Plus(cfg)
+    # model = DeepLabV3Plus(cfg)
+
+    # model = DPT(**{**model_configs[cfg['backbone'].split('_')[-1]], 'nclass': cfg['nclass']})
+
+    from mmengine.optim import build_optim_wrapper
+
+    optim_wrapper = dict(
+        optimizer=dict(
+            type="AdamW", lr=cfg["lr"], betas=(0.9, 0.999), weight_decay=0.01
+        ),
+        paramwise_cfg=dict(
+            custom_keys={
+                "absolute_pos_embed": dict(decay_mult=0.0),
+                "relative_position_bias_table": dict(decay_mult=0.0),
+                "norm": dict(decay_mult=0.0),
+            }
+        ),
+    )
+
+    optimizer = build_optim_wrapper(model, optim_wrapper)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer.optimizer, cfg["epochs"], eta_min=0, last_epoch=-1
+    )
+
+    if rank == 0:
+        logger.info("Total params: {:.1f}M\n".format(count_params(model)))
+
+    local_rank = int(os.environ["LOCAL_RANK"])
+    if ddp:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model.cuda()
+
+    if ddp:
+        model = torch.nn.parallel.DistributedDataParallel(
+            model,
+            device_ids=[local_rank],
+            broadcast_buffers=False,
+            output_device=local_rank,
+            find_unused_parameters=True,
+        )
+
+    if cfg["criterion"]["name"] == "CELoss":
+        criterion_l = nn.CrossEntropyLoss(**cfg["criterion"]["kwargs"]).cuda(local_rank)
+    elif cfg["criterion"]["name"] == "OHEM":
+        criterion_l = ProbOhemCrossEntropy2d(**cfg["criterion"]["kwargs"]).cuda(
+            local_rank
+        )
+    else:
+        raise NotImplementedError(
+            "%s criterion is not implemented" % cfg["criterion"]["name"]
+        )
+
+    criterion_u = nn.CrossEntropyLoss(reduction="none").cuda(local_rank)
+
+    trainset_u = SemiDataset(
+        cfg["dataset"],
+        cfg["data_root"],
+        "train_u",
+        size=cfg["crop_size"],
+        ignore_value=cfg["ignore_index"],
+        id_path=args.unlabeled_id_path,
+    )
+
+    trainset_l = SemiDataset(
+        cfg["dataset"],
+        cfg["data_root"],
+        "train_l",
+        size=cfg["crop_size"],
+        ignore_value=cfg["ignore_index"],
+        id_path=args.labeled_id_path,
+        nsample=len(trainset_u.ids),
+    )
+    valset = ValDataset(cfg["dataset"], cfg["data_root"], "val")
+
+    if ddp:
+        trainsampler_l = torch.utils.data.distributed.DistributedSampler(trainset_l)
+        trainloader_l = DataLoader(
+            trainset_l,
+            batch_size=cfg["batch_size"],
+            pin_memory=True,
+            num_workers=8,
+            drop_last=False,
+            sampler=trainsampler_l,
+        )
+        trainsampler_u = torch.utils.data.distributed.DistributedSampler(trainset_u)
+        trainloader_u = DataLoader(
+            trainset_u,
+            batch_size=cfg["batch_size"],
+            pin_memory=True,
+            num_workers=8,
+            drop_last=False,
+            sampler=trainsampler_u,
+        )
+        valsampler = torch.utils.data.distributed.DistributedSampler(valset)
+        valloader = DataLoader(
+            valset,
+            batch_size=4,
+            pin_memory=True,
+            num_workers=8,
+            drop_last=False,
+            sampler=valsampler,
+        )
+    else:
+        trainloader_l = DataLoader(
+            trainset_l,
+            batch_size=cfg["batch_size"],
+            pin_memory=True,
+            num_workers=8,
+            shuffle=True,
+            drop_last=True,
+        )
+        trainloader_u = DataLoader(
+            trainset_u,
+            batch_size=cfg["batch_size"],
+            pin_memory=True,
+            num_workers=1,
+            shuffle=True,
+            drop_last=True,
+        )
+        valloader = DataLoader(
+            valset, batch_size=1, pin_memory=True, num_workers=1, drop_last=False
+        )
+
+    total_epochs = cfg["epochs"]
+    total_iters = len(trainloader_u) * total_epochs
+    epoch = -1
+    previous_best = 0.0
+    ETA = 0.0
+    scaler = torch.cuda.amp.GradScaler()
+    is_best = False
+
+
+    # if os.path.exists('/data1/users/lvliang/project_123/SemiFT/exp/potsdam/fixmatch_rvsc/dinov3_base/1_4/latest.pth'):
+    #     checkpoint = torch.load(os.path.join(args.save_path, 'latest.pth'), map_location=torch.device('cpu'))
+    #     model.load_state_dict(checkpoint['model'])
+    #     optimizer.load_state_dict(checkpoint['optimizer'])
+    #     epoch = checkpoint['epoch']
+    #     previous_best = checkpoint['previous_best']
+
+    for epoch in range(epoch + 1, total_epochs):
+        start_time = time.time()
+        if rank == 0:
+            logger.info(
+                "===========> Epoch: {:}, LR: {:.5f}, Previous best: {:.2f}, ETA: {:.2f}M".format(
+                    epoch, optimizer.param_groups[0]["lr"], previous_best, ETA / 60
+                )
+            )
+
+        log_avg = DictAverageMeter()
+
+        if ddp:
+            trainloader_l.sampler.set_epoch(epoch)
+            trainloader_u.sampler.set_epoch(epoch)
+
+        start_time = time.time()
+        model.train()
+        total_loss = AverageMeter()
+        loader = zip(trainloader_l, trainloader_u)
+
+        for i, ((img_x , mask_x, img_x_c, mask_x_c),
+            (img_u_w, img_u_s, img_u_c, ignore_mask, cutmix_box, _, box, mask_c)) in enumerate(loader):
+
+            t0 = time.time()
+            img_x, mask_x = img_x.cuda(), mask_x.cuda()
+            img_x_c, mask_x_c = img_x_c.cuda(), mask_x_c.cuda()
+            img_u_w, img_u_s, img_u_c, ignore_mask = (
+                img_u_w.cuda(),
+                img_u_s.cuda(),
+                img_u_c.cuda(),
+                ignore_mask.cuda(),
+            )
+            mask_c = mask_c.cuda()
+            num_lb, num_ulb = img_x.shape[0], img_u_w.shape[0]
+
+            with torch.cuda.amp.autocast(enabled=amp):
+
+                model.train()
+                
+                pred_x = model(img_x)
+                pred_u_w = model(img_u_w)
+                pred_u_s = model(img_u_s)
+                pred_x_rvs = model(img_x_c)
+                pred_u_rvs = model(img_u_c)
+
+                pred_u_w = pred_u_w.detach()
+                conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
+                mask_u_w = pred_u_w.argmax(dim=1)
+                pred_u_w_rvs, mask_u_w_rvs, conf_u_w_rvs = pred_u_w.clone(), mask_u_w.clone(), conf_u_w.clone()
+                
+                pred_recovered, valid_masks = scale_back(
+                    pred_u_rvs, mask_c, cfg['crop_size'], box,
+                )
+                
+                valid_masks = valid_masks.squeeze(1)
+                mask_u_w_rvs[valid_masks==0] = cfg['criterion']['kwargs']['ignore_index']
+                
+                mask_u_w, conf_u_w = mask_u_w.clone(), conf_u_w.clone()
+                loss_x = criterion_l(pred_x, mask_x)
+                loss_x_rvs = criterion_l(pred_x_rvs, mask_x_c)
+                
+                loss_u_s = criterion_u(pred_u_s, mask_u_w)
+                loss_u_s = confidence_weighted_loss(
+                    loss_u_s,
+                    conf_u_w,
+                    ignore_mask,
+                    cfg["ignore_index"],
+                    conf_thresh=conf_thresh,
+                )
+                loss_u_s_rvs = criterion_l(pred_recovered, mask_u_w_rvs)
+                loss_u_s_rvs = confidence_weighted_loss(
+                    loss_u_s_rvs,
+                    conf_u_w,
+                    mask_u_w_rvs,
+                    cfg['criterion']['kwargs']['ignore_index'],
+                    conf_thresh=conf_thresh,
+                )
+                mask_ratio = (
+                    (conf_u_w >= conf_thresh) & (ignore_mask != cfg["ignore_index"])
+                ).sum().item() / (ignore_mask != cfg["ignore_index"]).sum()
+                total_loss = (loss_x + loss_x_rvs + loss_u_s + loss_u_s_rvs) / 4.0
+
+            if ddp:
+                torch.distributed.barrier()
+
+            optimizer.zero_grad()
+            if amp:
+                loss = scaler.scale(total_loss)
+                # torch.autograd.set_detect_anomaly(True)
+                loss.backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                total_loss.backward()
+                optimizer.step()
+
+            # Logging
+            log_avg.update(
+                {
+                    "iter time": time.time() - t0,
+                    "Total loss": total_loss,  # Logging original loss (=total loss), not AMP scaled loss
+                    "Loss x": loss_x,
+                    "Loss x_rvs": loss_x_rvs,
+                    "Loss u_s": loss_u_s,
+                    "Loss u_s_rvs": loss_u_s_rvs,
+                    "Mask ratio": mask_ratio,
+                }
+            )
+
+            iters = epoch * len(trainloader_u) + i
+            if (i % (len(trainloader_u) // 8) == 0) and (rank == 0):
+                logger.info(f"Iters: {i}, " + str(log_avg))
+                log_avg.reset()
+
+        if epoch % args.interval == 0:
+            start_time = time.time()
+            mIoU, mAcc, mF1, allAcc, iou_class, F1_class = validation_cpu(
+                cfg, model, valloader
+            )
+            end_time = time.time()
+
+            if rank == 0:
+                for cls_idx, iou in enumerate(iou_class):
+                    logger.info(
+                        "***** Evaluation ***** >>>> Class [{:} {:}] "
+                        "IoU: {:.2f}".format(
+                            cls_idx, CLASSES[cfg["dataset"]][cls_idx], iou
+                        )
+                    )
+                # logger.info('***** Evaluation {} ***** >>>> MeanIoU: {:.2f}\n'.format(eval_mode, mIoU))
+                logger.info(
+                    "Last: validation epoch [{}/{}]: mIoU/mAcc/mF1/allAcc {:.4f}/{:.4f}/{:.4f}/{:.4f}. Cost {:.4f} secs".format(
+                        epoch + 1,
+                        cfg["epochs"],
+                        mIoU,
+                        mAcc,
+                        mF1,
+                        allAcc,
+                        end_time - start_time,
+                    )
+                )
+
+                writer.add_scalar("eval/mIoU", mIoU, epoch)
+                for i, iou in enumerate(iou_class):
+                    writer.add_scalar(
+                        "eval/%s_IoU" % (CLASSES[cfg["dataset"]][i]), iou, epoch
+                    )
+
+            is_best = mIoU > previous_best
+            previous_best = max(mIoU, previous_best)
+            if rank == 0:
+                checkpoint = {
+                    "model": model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "epoch": epoch,
+                    "previous_best": previous_best,
+                }
+                torch.save(checkpoint, os.path.join(args.save_path, "latest.pth"))
+                if is_best:
+                    torch.save(checkpoint, os.path.join(args.save_path, "best.pth"))
+        scheduler.step()
+
+
+if __name__ == "__main__":
+    set_seeds(1234)
+    main()
