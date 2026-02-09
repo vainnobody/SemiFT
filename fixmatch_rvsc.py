f8440d9a219d314f0578b7e900924ee30196d62a
lanjie <1355008426@qq.com>
2026-02-09 10:39:29 +0800

    [debug] rgcr方法消融

--- fixmatch_rvsc.py
+++ fixmatch_rvsc.py
@@ -488,7 +488,7 @@ def main(args, cfg):
             # Total loss
             # loss_x_rvs is detached to not participate in backward pass, but still logged
             loss = (
-                loss_x + loss_x_rvs.detach() + loss_u_s * 0.5 + loss_u_s_rvs * 0.5
+                loss_x + loss_x_rvs.detach() + loss_u_s * 1.0 + loss_u_s_rvs * 0.5
             ) / 2.0
 
             torch.distributed.barrier()
