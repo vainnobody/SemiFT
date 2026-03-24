c341ff84903fb0ff302f7dd2b0a6b5d8d08e0ada
lanjie <1355008426@qq.com>
2026-03-24 14:12:14 +0800

    [cleanup] remove segmind code

--- test/test_rn101_backbone_support.py
+++ test/test_rn101_backbone_support.py
@@ -25,7 +25,6 @@ stub_tb.SummaryWriter = StubSummaryWriter
 sys.modules.setdefault("torch.utils.tensorboard", stub_tb)
 
 from model.semseg import dpt as dpt_mod
-from model.semseg import dpt_segmind as dpt_segmind_mod
 from model.semseg import upernet as upernet_mod
 from model.backbone.resnet import ResNet101Backbone
 from util import ssl_method_utils as ssl_utils
@@ -57,7 +56,6 @@ class FakeResNet101Backbone(torch.nn.Module):
 @pytest.fixture(autouse=True)
 def patch_fake_resnet(monkeypatch):
     monkeypatch.setattr(dpt_mod, "ResNet101Backbone", FakeResNet101Backbone)
-    monkeypatch.setattr(dpt_segmind_mod, "ResNet101Backbone", FakeResNet101Backbone)
     monkeypatch.setattr(upernet_mod, "ResNet101Backbone", FakeResNet101Backbone)
 
 
@@ -155,22 +153,6 @@ def test_dpt_supports_resnet101_need_fp():
     y, y_fp = model(x, need_fp=True)
     assert y.shape == (2, 3, 128, 128)
     assert y_fp.shape == (2, 3, 128, 128)
-
-
-def test_dpt_segmind_supports_resnet101_aux_outputs():
-    model = dpt_segmind_mod.DPT_SegMind(
-        encoder_size="resnet101",
-        nclass=5,
-        features=64,
-        out_channels=[256, 512, 1024, 2048],
-        backbone_version="resnet",
-        proj_dim=16,
-    )
-    x = torch.randn(2, 3, 128, 128)
-    logits, proj_feat, recon = model(x, return_aux=True)
-    assert logits.shape == (2, 5, 128, 128)
-    assert proj_feat.shape[-2:] == (32, 32)
-    assert recon.shape == (2, 3, 128, 128)
 
 
 def test_scalematch_models_support_resnet101_backbone():
