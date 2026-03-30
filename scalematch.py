20959884711a8d85d1cdafce98be72e3c5981c77
lanjie <1355008426@qq.com>
2026-03-30 21:17:18 +0800

    [scalematch] fix ddp reduction and hang issues

--- scalematch.py
+++ scalematch.py
@@ -341,10 +341,13 @@ def main(args, cfg):
             cutmix_img_(img_u_s2, img_u_s2_mix, cutmix_box2)
 
             with torch.cuda.amp.autocast(enabled=amp):
-                model.eval()
-                pred_u_w_mix = model(img_u_w_mix, scale_factor=None, scales=None)
-                pred_u_w_mix = pred_u_w_mix.detach()
-                conf_u_w_mix, mask_u_w_mix = pred_u_w_mix.softmax(dim=1).max(dim=1)
+                inference_model = model.module if hasattr(model, "module") else model
+                inference_model.eval()
+                with torch.no_grad():
+                    pred_u_w_mix = inference_model(
+                        img_u_w_mix, scale_factor=None, scales=None
+                    )
+                    conf_u_w_mix, mask_u_w_mix = pred_u_w_mix.softmax(dim=1).max(dim=1)
 
                 model.train()
 
@@ -403,8 +406,6 @@ def main(args, cfg):
 
                 loss_standard = loss_u_s1 * 0.25 + loss_u_size * 0.25 + loss_u_w_fp * 0.5
                 total_loss = (loss_x + loss_standard) / 2.0
-
-            torch.distributed.barrier()
 
             optimizer.zero_grad()
             if amp:
